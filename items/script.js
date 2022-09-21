21613f78996cd755e2fbad03df75cffe7b9245db
DaScOrPiO <dunni82@yahoo.com>
2022-09-21 00:00:50 -0700

    Api research commit

--- items/script.js
+++ items/script.js
@@ -1,14 +1,29 @@
 let slideIndex = 0;
+let slide = document.getElementsByClassName('slider');
 const animation = () => {
-    let slide = document.getElementsByClassName('slider');
-    for (let i = 0; i < slide.length; i++) {
-        slide[i].style.display = 'none'
+    if (slide) {
+        for (let i = 0; i < slide.length; i++) {
+            slide[i].style.display = 'none'
+        }
+        slideIndex++
+        if (slideIndex > slide.length) {
+            slideIndex = 1;
+        }
+        slide[slideIndex - 1].style.display = 'block';
+        setTimeout(animation, 4000);
     }
-    slideIndex++
-    if (slideIndex > slide.length) {
-        slideIndex = 1;
-    }
-    slide[slideIndex - 1].style.display = 'block';
-    setTimeout(animation, 4000);
 }
-animation();
+document.addEventListener('DOMContentLoaded', animation)
+// animation();
+const input = document.querySelector('#form1');
+const movieSearchBtn = document.querySelector('#button1');
+const seriesSearchBtn = document.querySelector('#button2');
+
+const searchMovies = async () => {
+    movieSearchBtn.addEventListener('click', function () {
+        console.log(input.value);
+    })
+    // let res = await axios.get(`https://api.tvmaze.com/singlesearch/shows?q=girls`);
+    // console.log(res.data);
+}
+movieSearchBtn.addEventListener('click', searchMovies)
